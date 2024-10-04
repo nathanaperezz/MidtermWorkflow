218f64273f6fa506da5331445e169489531ec619
Nathan Perez <naperezmail@gmail.com>
2024-10-04 19:59:29 -0400

    seperated random dice number into its own method

--- src/main/java/org/example/DiceRoller.java
+++ src/main/java/org/example/DiceRoller.java
@@ -3,14 +3,12 @@ package org.example;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class DiceRoller {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        Random random = new Random();
 
         while(true) {
-            System.out.print("Press Enter to roll a dice or -1 to quit: ");
+            System.out.print("Press enter to roll a dice or -1 to quit: ");
             String input = scan.nextLine();
 
             if(input.equals("-1")) {
@@ -18,10 +16,16 @@ public class Main {
             }
 
             else {
-                int roll = random.nextInt(6) + 1;
+                int roll = getRoll();
                 System.out.println("You rolled a " + roll);
             }
         }
         System.out.println("Goodbye!");
+    }
+
+
+    static int getRoll(){
+        Random random = new Random();
+        return random.nextInt(6) + 1;
     }
 }
