5a6aa8b57d18632f6a33b37bdfba457ee79f36e8
Nathan Perez <naperezmail@gmail.com>
2024-10-04 13:46:56 -0400

    added dice roller

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+
+        while(true) {
+            System.out.print("Press Enter to roll a dice or -1 to quit: ");
+            String input = scan.nextLine();
+
+            if(input.equals("-1")) {
+                break;
+            }
+
+            else {
+                int roll = random.nextInt(6) + 1;
+                System.out.println("You rolled a " + roll);
+            }
+        }
+        System.out.println("Goodbye!");
     }
 }
